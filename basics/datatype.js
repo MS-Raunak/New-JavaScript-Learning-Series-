58050ee7e791a733f179a990c9e6bf8bec7a5b4c
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 01:29:48 +0530

    JS:Data Type

--- basics/datatype.js
+++ basics/datatype.js
@@ -5,33 +5,40 @@
  * 2) Non-Primitive=>Array, Objects
  */
 
-//Number
-let a=10;
-console.log(typeof(a));//number
-a=10.2;
-console.log(typeof(a));//number
+// //Number
+// let a=10;
+// console.log(typeof(a));//number
+// a=10.2;
+// console.log(typeof(a));//number
 
-//String: In Js we can write a string either within double quotes or single quotes
-let str = "Chhaya";
-console.log(typeof(str));//string
-str = 'Chhaya';
-console.log(typeof(str));//string
-str = 'r';
-console.log(typeof(str));//string
+// //String: In Js we can write a string either within double quotes or single quotes
+// let str = "Chhaya";
+// console.log(typeof(str));//string
+// str = 'Chhaya';
+// console.log(typeof(str));//string
+// str = 'r';
+// console.log(typeof(str));//string
 
-//Boolean
-console.log(typeof(true));//boolean
+// //Boolean
+// console.log(typeof(true));//boolean
 
-//Undefined: varible which is only declared but not initialized
-let num;
-console.log(typeof(num)); //undefined
+// //Undefined: varible which is only declared but not initialized
+// let num;
+// console.log(typeof(num)); //undefined
 
-//Null
-num = null;
-console.log(typeof(num)); //object(Non-primitive): this is a bug of javascript
+// //Null
+// num = null;
+// console.log(typeof(num)); //object(Non-primitive): this is a bug of javascript
 
-//BigInt: to store longest value
-let val = BigInt(134566666666634212234); //First way to assigne bigint val
-let val1 = 445565652n; // //Second way to assigne bigint val
+// //BigInt: to store longest value
+// let val = BigInt(134566666666634212234); //First way to assigne bigint val
+// let val1 = 445565652n; // //Second way to assigne bigint val
 
-console.log(val + val1);
+// console.log(val + val1);
+
+//Tricky Example
+let t = "2";
+console.log(typeof t); //string
+t = +"2"; //+ convert string 2 into number
+console.log(typeof t); //number
+
