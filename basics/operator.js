601a2647df830f1e3098a1ef9863dda2df458ca2
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 02:03:22 +0530

    JS:Ternary Operator

--- basics/operator.js
+++ basics/operator.js
@@ -1,10 +1,15 @@
 //Logical , Comparison
 
-let a = "10";
-let b=10;
+// let a = "10";
+// let b=10;
 
-console.log(a==b);//true, Since == compare value only but not datatype
-console.log(a===b);//false, Since === compares both value and datatype as well as.
+// console.log(a==b);//true, Since == compare value only but not datatype
+// console.log(a===b);//false, Since === compares both value and datatype as well as.
 
-console.log(a!=b);//false
-console.log(a!==b); //true
+// console.log(a!=b);//false
+// console.log(a!==b); //true
+
+
+//Ternary Operator
+let age = 18;
+age<18?console.log("Your are Minor"):console.log("Not Minor");;
