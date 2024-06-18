bdaa00ffa2cd3d85ea85382b984748cc3c7bb0cd
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 00:18:55 +0530

    JS: Variable without kyeword

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+/**
+ * Variable: It is a container which can hold at most one value
+ */
+
+name = "Ritesh";
+console.log(name);
+
+Name = "Chhaya";
+console.log(name);
+
+//name and Name are two different variable 
+//Above variable declaration is not recommended
