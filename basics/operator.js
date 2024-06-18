335dafd86a206dd21fe0967bdfde2282575a97b0
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 01:39:29 +0530

    JS:Opertor, ==, === comparison oparator

---
+++ basics/operator.js
@@ -0,0 +1,10 @@
+//Logical , Comparison
+
+let a = "10";
+let b=10;
+
+console.log(a==b);//true, Since == compare value only but not datatype
+console.log(a===b);//false, Since === compares both value and datatype as well as.
+
+console.log(a!=b);//false
+console.log(a!==b); //true
