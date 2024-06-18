f288d7d7b0a29cf7331f65b518151c8e9a147fc6
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 00:59:02 +0530

    JS:Rule to Define Variable

--- basics/variables.js
+++ basics/variables.js
@@ -23,9 +23,32 @@
 // //nickname = "CHH";     //Throwing error bkg const type variable neihther redeclare nor reassigned
 
 
-var a=10;
-var b=20;
+// var a=10;
+// var b=20;
 
-console.log("a: ", a); //10
-console.log("a+b: ", a+b); //30
+// console.log("a: ", a); //10
+// console.log("a+b: ", a+b); //30
+
+/** Rule to declare JS Variables 
+ * - We can't use special chars except underscore(_) and doller($)
+ * - we cant use number as starting character in variable declaration
+ * - we can use naming conventions to declare any variables(Recommended) but it is optional
+ * 
+*/
+
+var  _a= 10; //correct
+var  $a= 10; //correct
+////var  @a= 10; //error
+////var 1a=10; //error
+var a1=10; //correct
+
+//Naming Conventions
+//1. normal
+var hi = "hello";
+//2.snake_case
+var hi_hello = "morning";
+//3.camelCase
+var hiHello = "morning";
+
+
 
