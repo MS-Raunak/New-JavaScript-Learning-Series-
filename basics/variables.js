819bd06b388c54f55883d833c82ee6f6f6dcd065
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 00:27:10 +0530

    JS: strict mode

--- basics/variables.js
+++ basics/variables.js
@@ -1,12 +1,15 @@
-/**
- * Variable: It is a container which can hold at most one value
- */
+
+//If we use strict mode here then we can't declare variable without keyword(var,let,const)
+"use strict";
 
 name = "Ritesh";
 console.log(name);
+//Now if we executing this program then we will get ReferenceError: name is not defined
 
-Name = "Chhaya";
-console.log(name);
-
-//name and Name are two different variable 
-//Above variable declaration is not recommended
+/**
+ * Stric Mode
+ * ==========
+ * Using "use strict"; in JavaScript is like turning on a safety feature for your code. 
+ * It makes JavaScript more strict about how you write your code, which helps catch mistakes early. 
+ * EX: consider above example with const,var,let we can't declare the variale
+ */
