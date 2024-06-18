ccb19e8f37d363bfc2d567621de569814ed2d2f0
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 01:44:27 +0530

    JS:conditional-statement

---
+++ basics/conditionalstate.js
@@ -0,0 +1,19 @@
+// let age = 20;
+
+// if(age < 18){
+//     console.log("Your are minor");
+// }
+// else{
+//     console.log("You are not Minor");
+// }
+
+let key = 'a';
+switch (key) {
+    case 'a':
+        console.log("Got a");
+        break;
+
+    default:
+        console.log("I did'nt get a");
+        break;
+}
