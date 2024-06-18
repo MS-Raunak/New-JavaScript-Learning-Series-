d72c0d9b57721d0a6e28d61af27187ddbc71fe21
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 00:12:01 +0530

    JS: Introduction(Theory)

--- basics/hello-world.js
+++ basics/hello-world.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+/**
+ * What is JavaScript?
+ * JavaScript is a client-side scripting language which is used to make dynamic webpage.
+ * 
+ * Features:
+ * 1) JS is a high level programming language.
+ * 2) JS is dynamically typed programming language.
+ * 3)JavaScript is a case-sensitive language. This means that language keywords, variables, function names, 
+ * and any other identifiers must always be typed with a consistent capitalization of letters.
+ * 4) JavaScript Supports both procedural and oops programming paradigm.
+ * 5) Js is a single threaded programming language.
+ */
