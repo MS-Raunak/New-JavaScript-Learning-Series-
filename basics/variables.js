5d3bcec876b849e8bd14d8d668bd36a35f5ec662
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-19 00:48:33 +0530

    JS:Variable example

--- basics/variables.js
+++ basics/variables.js
@@ -8,18 +8,24 @@
  * For at this time don't care about difference, You will get to know later soon
  */
 
-var name = "Ritesh";  
-console.log(name); //Ritesh
-var name = "Shanaya"; //here same variable 'name' , has been declared twice because of var type variables we can redeclare
-console.log(name); //Shanaya
+// var name = "Ritesh";  
+// console.log(name); //Ritesh
+// var name = "Shanaya"; //here same variable 'name' , has been declared twice because of var type variables we can redeclare
+// console.log(name); //Shanaya
 
-let age = 10;
-console.log(age);//10
-age=20;       //let type variable we can reassigned but cant be redeclare
-console.log(age);//20
+// let age = 10;
+// console.log(age);//10
+// age=20;       //let type variable we can reassigned but cant be redeclare
+// console.log(age);//20
 
-const nickname = "Chhaya";
-console.log(nickname);
-//nickname = "CHH";     //Throwing error bkg const type variable neihther redeclare nor reassigned
+// const nickname = "Chhaya";
+// console.log(nickname);
+// //nickname = "CHH";     //Throwing error bkg const type variable neihther redeclare nor reassigned
 
+
+var a=10;
+var b=20;
+
+console.log("a: ", a); //10
+console.log("a+b: ", a+b); //30
 
