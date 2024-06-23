232fdac772f65b766a72ff88ca6de698928d038d
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-23 16:07:19 +0530

    default-parameter in js

---
+++ basics/defaultparam.js
@@ -0,0 +1,9 @@
+function name(num=10) {
+    console.log(num);
+}
+
+name();
+name(20);
+
+//In above function the default parameter num=10, if we don't pass any value then it will print default value
+//if we pass any values then it will print passing value
