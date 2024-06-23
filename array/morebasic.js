998275e6c376d586f448436c1fd1962c433f650e
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-23 18:25:41 +0530

    medium function in js

---
+++ array/morebasic.js
@@ -0,0 +1,20 @@
+let arr= [1,2,3,4,5];
+
+//#####includes() return boolean value whether the given ele present in existing arr or not
+//console.log(arr.includes(5));//true
+//console.log(arr.includes(50));//false
+
+//#####indexOf() return index val if given ele present if not present then it will return -1
+//console.log(arr.indexOf(1));//0th index
+//console.log(arr.indexOf(10));//-1
+
+//######at(index) take index val and return ele on that position
+//console.log(arr.at(4)); //5
+
+//######concat() used to concat multiple array together in single new array
+//let arr2 = [7,9,6,8];
+//console.log(arr.concat(arr2));
+
+//#####
+
+
