051e8689c60766d0525f81e4b63f2e8b131adacf
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-23 18:29:00 +0530

    spread operator

---
+++ array/spreadOp.js
@@ -0,0 +1,10 @@
+let arr1 = [1,2,3,4,5];
+let arr2 = [5,3,6,7,89,9];
+
+let resArr = [...arr1,...arr2];
+console.log(resArr);
+
+/**
+ * spread operator is used to spread or merge or combine two or more than two array into a single array same like
+ * concat function.
+ */
