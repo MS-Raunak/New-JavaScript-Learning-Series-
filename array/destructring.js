4a0b19aa04001c817e313dc6bfd8fa74d805e00c
MS-Raunak <raunakkr.saha@gmail.com>
2024-06-28 20:17:25 +0530

    new comment about destructuring

--- array/destructring.js
+++ array/destructring.js
@@ -1,6 +1,12 @@
 /**
  * Destructuring: is the mechanism to extracting value from array or object and 
  * assigning to the convinient variables
+ * 
+ * There are two types of destructuring 
+ * 1.Array Destructuring
+ * 2.Object Destructuring
+ * 
+ * In this file we are going to see Array Destructuring
  */
 const colors = ["red", "blue", "white", "green", "yellow"];
 //array destructuring
