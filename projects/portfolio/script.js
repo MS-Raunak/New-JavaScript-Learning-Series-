20d6f4b87c0eb19de9cd96cd9f67bec54dc2e13f
MS-Raunak <raunakkr.saha@gmail.com>
2024-07-05 01:10:46 +0530

    portfolio project using html,css,js

---
+++ projects/portfolio/script.js
@@ -0,0 +1,6 @@
+
+        var typed = new Typed('#element', {
+          strings: ['Java Developer', 'Full Stack Developer', 'Front end Developer', 'Backend Developer', 'Web Designer', 'Android Developer'],
+          typeSpeed: 50,
+        });
+     
